5c3c288843b3ed77c7cd999414bf7452d4a4689f
lu <@>
2017-05-18 16:51:17 +0800

    init

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -9,7 +9,7 @@ function resolve (dir) {
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: './src/weixin/app.js'
   },
   output: {
     path: config.build.assetsRoot,
@@ -21,8 +21,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src')
+      'vue$': 'vue/dist/vue.esm.js'
     }
   },
   module: {
