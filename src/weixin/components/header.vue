7eb7b0fb11ab39250c755e89d7f36dfc1175ae7f
msc <956164483@qq.com>
2017-05-20 01:43:52 +0800

    1

---
+++ src/weixin/components/header.vue
@@ -0,0 +1,88 @@
+<template>
+    <header class="box box-ac header">
+        <div class="icon-user" @click="checkLogin('./userList')">
+            <div v-if="hasNews" class="news"></div>
+        </div>
+        <form class="box box-f1 box-ac" action="javascript:return true;" >
+            <div class="icon-search"></div>
+            <input id="search" type="search" class="box box-f1" placeholder="请输入您要搜索的话题" />
+        </form>
+        <div class="icon-user-list " @click="checkLogin('./userList')">
+        </div>
+    </header>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isLogin: false,
+      hasNews: true
+    }
+  },
+  methods: {
+    checkLogin(router) {
+      var self = this
+      if (!self.isLogin) {
+        self.$router.push('./login')
+      } else {
+        self.$router.push(router)
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+    @import '../css/color.scss';
+    @import '../css/mixins.scss';
+
+    .header{
+        height:pxToRem(110);
+        padding-left:pxToRem(20);
+        padding-right:pxToRem(20);
+        @include border-1px($border-color);
+        form{
+            margin-left:pxToRem(30);
+            margin-right:pxToRem(30);
+            padding-right:pxToRem(30);
+            padding-left:pxToRem(30);
+            border-radius:pxToRem(66);
+            border:pxToRem(2) solid $theme-color;
+            height:pxToRem(66);
+            overflow:hidden;
+            .icon-search{
+                @include bg-image('../img/icon-search');
+                @include bg-size(cover);
+                width:pxToRem(48);
+                height:pxToRem(48);
+            }
+            #search{
+                margin-left:pxToRem(10);
+                font-size:pxToRem(30);
+            }
+        }
+    }
+
+   .icon-user,.icon-user-list{
+        width: pxToRem(58);
+        height: pxToRem(58);
+        @include bg-size(cover);
+   }
+   
+   .icon-user{
+       .news{
+           position:absolute;
+           width:pxToRem(10);
+           height:pxToRem(10);
+           border-radius:50%;
+           background-color: #e60012;
+           right:pxToRem(-5);
+           top:pxToRem(-5);
+       }
+       position:relative;
+       @include bg-image('../img/icon-user');
+   }
+   .icon-user-list{
+       @include bg-image('../img/icon-user-list');
+   }
+</style>
+
---
+++ src/weixin/views/topicList.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="box box-ver">
+        <div class="box box-ac tab">
+            <div :class="actIndex==0?'act':''" class="tab-item box box-f1 box-fh box-ac box-pc">
+                <div>高校</div>
+                <div class="arrow icon-arrow-bot icon-arrow-top-act"></div>
+            </div>
+            <div :class="actIndex==1?'act':''" class="tab-item box box-f1 box-fh box-ac box-pc">
+                <div>院系</div>
+                <div class="arrow icon-arrow-bot icon-arrow-top-act"></div>
+            </div>
+            <div :class="actIndex==2?'act':''" class="tab-item box box-f1 box-fh box-ac box-pc">
+                <div>专业</div>
+                <div class="arrow icon-arrow-bot icon-arrow-top-act"></div>
+            </div>
+        </div>
+        <div class="box box-f1 box-ver">
+            <div class="box topic-item">
+
+            </div>
+            <div class="box topic-item">
+                
+            </div>
+            <div class="box topic-item">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  components: {
+    'v-header': require('../components/header.vue')
+  },
+  data () {
+    return {
+      actIndex: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+    @import '../css/color.scss';
+    @import '../css/mixins.scss';
+    .arrow{
+        width: pxToRem(30);
+        height: pxToRem(16);
+        @include bg-size(cover);
+        margin-left:pxToRem(30);
+    }
+    .icon-arrow-bot{
+        @include bg-image('../img/icon-arrow-bot');
+    }
+    .tab{
+        height:pxToRem(72);
+        @include border-1px($border-color);
+        .tab-item{
+            border-right:2px solid $theme-color;
+            &:last-child{
+                border:0;
+            }
+            &.act{
+                color:$theme-color;
+                .icon-arrow-bot{
+                    @include bg-image('../img/icon-arrow-top-act');
+                }
+            }
+        }
+        
+    }
+    .topic-item{
+        margin:pxToRem(10) pxToRem(20);
+        height:pxToRem(480);
+        background-color: #999;
+    }
+</style>
