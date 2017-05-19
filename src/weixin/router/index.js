549f8fcd74e40aed60913e5352d49babab3f28d1
msc <956164483@qq.com>
2017-05-20 01:42:52 +0800

    统一命名

--- src/weixin/router/index.js
+++ src/weixin/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/home.vue'
+import topicList from '../views/topicList.vue'
 import Login from '../views/login.vue'
 
 Vue.use(Router)
@@ -9,7 +10,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: Home
+      component: Home,
+      children: [
+        {
+          path: '/',
+          component: topicList
+        }
+      ]
     },
     {
       path: '/login',
