089656767e1f5ea66aa8d0ed86acb3a78e2bd6b3
lu <@>
2017-05-20 14:57:10 +0800

    添加注释

--- src/weixin/views/App.vue
+++ src/weixin/views/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
+  <div class="root">
+    <!-- header用于开发时模拟页面高度，手机上打开时请删掉 -->
     <header style="height:130px;background:#eee;position:fixed;top:0;width: 100%;z-index: 999;"></header>
-    <div class="container" style="margin-top:130px;">
+    <div class="container">
       <router-view></router-view>
     </div>
   </div>
@@ -15,3 +16,8 @@ import '../css/mint-ui.scss'
 
 export default {}
 </script>
+
+<style scoped>
+/*用于开发时模拟页面高度，手机上打开时请删掉*/
+  .container {margin-top:130px;}
+</style>
