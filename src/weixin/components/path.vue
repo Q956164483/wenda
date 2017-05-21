b3a5b5555ec497d2d5703c6b5dcc85a7f1eb533f
msc <956164483@qq.com>
2017-05-21 16:59:11 +0800

    22

--- src/weixin/components/path.vue
+++ src/weixin/components/path.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="path">
       <i class="comicon-17 icon1-return" @click="$router.go(-1)"></i>
-      <a href="javascript:void(0);" @click="$router.push('/')">首页</a>
-      &gt;
       <slot></slot>
       <span>{{current}}</span>
   </div>
